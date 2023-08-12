9a4d6a57ccf1fc84bfb600e1366ba8bd62e1f6d5
LissNikita <dragonlisov@mail.ru>
2023-08-12 10:18:00 +0300

    add PropertiesReader

---
+++ src/main/java/org/fortvision/utils/PropertiesReader.java
@@ -0,0 +1,26 @@
+package org.fortvision.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+public class PropertiesReader {
+    public static String getPropertyValue(String propertyName) {
+
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = new FileInputStream("src/main/resources/properties.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        if (properties.getProperty(propertyName) != null) {
+            return properties.getProperty(propertyName);
+        } else {
+            throw new NoSuchElementException(propertyName + " is not found");
+        }
+    }
+}
