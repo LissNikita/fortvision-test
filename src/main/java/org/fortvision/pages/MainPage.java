e90e054bf7bde818ce2dc2d1e7f629b7e0223b38
LissNikita <dragonlisov@mail.ru>
2023-08-12 11:03:49 +0300

    add testNG.xml + selectors(MainPage)

--- src/main/java/org/fortvision/pages/MainPage.java
+++ src/main/java/org/fortvision/pages/MainPage.java
@@ -1,4 +1,15 @@
 package org.fortvision.pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class MainPage {
+
+    public SelenideElement titleLogo = $x("//img[@class = 'custom-logo']");
+    public SelenideElement loginButton = $x("//a[@title = 'Login']");
+    public SelenideElement solutionsButton = $(By.id("menu-item-234"));
+    public SelenideElement marketingAutomationButton = $(By.id("menu-item-53"));
 }
